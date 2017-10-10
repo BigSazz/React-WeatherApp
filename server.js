fce22df8633625ede3f7ac60e026b8610c03d5f5
Osaze Oviawe <ocubed88@gmail.com>
2017-10-10 10:06:46 +0100

    Server.js updated

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Server stated on port: ${PORT}` );
 });
