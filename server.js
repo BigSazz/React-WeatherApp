37123708b3ae04f768b98d93183da2e01f1a916b
Osaze Oviawe <ocubed88@gmail.com>
2017-10-10 09:52:03 +0100

    Heroku prep

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+// converting https to http
+app.use((req, res, next) => {
+    if (res.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log('Server stated on port 3000');
+app.listen(port, () => {
+    console.log(`Server stated on port: ${port}` );
 });
